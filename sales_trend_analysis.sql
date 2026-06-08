7fdddff35cc2172df2d825a9d1577abcbab3ea9f
Aishwarya <aishwaryavs661@gmail.com>
2026-06-08 21:21:04 +0530

    Create sales_trend_analysis.sql

---
+++ sales_trend_analysis.sql
@@ -0,0 +1,8 @@
+SELECT
+    strftime('%Y', order_date) AS year,
+    strftime('%m', order_date) AS month,
+    SUM(amount) AS total_revenue,
+    COUNT(DISTINCT order_id) AS order_volume
+FROM online_sales
+GROUP BY year, month
+ORDER BY year, month;
